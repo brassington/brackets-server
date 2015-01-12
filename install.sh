0f33bc28a98d3720afdff421bdbe14287dfb2212
J. Andrew Brassington <jabbrass@zoho.com>
2015-01-11 18:29:07 -0800

    Add git hard reset on specific version

--- install.sh
+++ install.sh
@@ -7,11 +7,12 @@ git submodule update --recursive --init
 # Target specific commit version by checking out from brackets-src submodule
 cd brackets-src/
 git checkout ea908cae55233d34f04b4f2cab5faf62ffa4fb42
+git reset HEAD --hard
 cd ../
 
 # Replace CodeMirror2 directly from repo
-sudo rm -rf ./brackets-src/src/thirdparty/CodeMirror2
-git clone https://github.com/codemirror/CodeMirror ./brackets-src/src/thirdparty/CodeMirror2 --recursive
+# sudo rm -rf ./brackets-src/src/thirdparty/CodeMirror2
+# git clone https://github.com/codemirror/CodeMirror ./brackets-src/src/thirdparty/CodeMirror2 --recursive
 
 # Fix blocking build issue TODO: Why does Brackets commit: 5742874a7449525488535679b2b2fef009bdfed9 cause LiveDev blocking issue?
 wget -O ./brackets-src/src/LiveDevelopment/LiveDevelopment.js https://raw.githubusercontent.com/adobe/brackets/9b92198f962d273231ac6b399fd7649cb9a5a37b/src/LiveDevelopment/LiveDevelopment.js
