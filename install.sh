6b4259a469bd5ed163c4090fc4c513bf940e14d3
J. Andrew Brassington <jabbrass@zoho.com>
2015-01-11 17:55:05 -0800

    Fix broken install script / Add custom extensions

--- install.sh
+++ install.sh
@@ -1,7 +1,38 @@
-sudo npm install
-git submodule update --init
-sudo rm-rf ./brackets-src/src/thirdparty/CodeMirror2
+# Refer to CONTRIBUTING.md for more detail on build steps:
+# https://github.com/rabchev/brackets-server/blob/master/CONTRIBUTING.md
+
+# Grab submodules
+git submodule update --recursive --init
+
+# Target specific commit version by checking out from brackets-src submodule
+cd brackets-src/
+git checkout ea908cae55233d34f04b4f2cab5faf62ffa4fb42
+cd ../
+
+# Replace CodeMirror2 directly from repo
+sudo rm -rf ./brackets-src/src/thirdparty/CodeMirror2
 git clone https://github.com/codemirror/CodeMirror ./brackets-src/src/thirdparty/CodeMirror2 --recursive
-sed -i '' 's%#grid > .core > .span(@gridColumns);%//#grid > .core > .span(@gridColumns);%' ./brackets-src/src/styles/bootstrap/navbar.less 
+
+# Fix blocking build issue TODO: Why does Brackets commit: 5742874a7449525488535679b2b2fef009bdfed9 cause LiveDev blocking issue?
+wget -O ./brackets-src/src/LiveDevelopment/LiveDevelopment.js https://github.com/adobe/brackets/blob/9b92198f962d273231ac6b399fd7649cb9a5a37b/src/LiveDevelopment/LiveDevelopment.js
+
+# Install NPM dependencies
+sudo npm install
+
+# Fix blocking build issue (Does not work on OSX, sed issue?) TODO: Why is this line a problem?
+sed -i 's%#grid > .core > .span(@gridColumns);%//#grid > .core > .span(@gridColumns);%' ./brackets-src/src/styles/bootstrap/navbar.less
+
+# Run Grunt build, to install grunt: npm install -g grunt-cli
 sudo grunt build
- 
+
+# Clone down copies of custom/default installed extensions
+cd embedded-ext/
+git clone https://github.com/jabbrass/brackets-working-file-tabs.git
+cd ../
+
+# Use this command to start the server from the build directory
+# node bin/run -d
+#
+# or use this command set of commands to remove you current global brackets-server installation
+# npm uninstall -g brackets
+# npm install -g
